75ebd0ce6de35f5d757ed7061ab7094df3236c01
Tacio Costa <taciomcosta@hotmail.com>
2018-05-15 23:28:02 -0300

    create sql script na pasta res

---
+++ res/bebida.sql
@@ -0,0 +1,63 @@
+﻿CREATE DATABASE mobywine;
+USE mobywine;
+-- phpMyAdmin SQL Dump
+-- version 4.2.7.1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: 16-Maio-2018 às 02:22
+-- Versão do servidor: 5.6.20-log
+-- PHP Version: 5.4.31
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `mobywine`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `bebida`
+--
+
+CREATE TABLE IF NOT EXISTS `bebida` (
+`id` int(11) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `pais` varchar(100) NOT NULL,
+  `regiao` varchar(100) NOT NULL,
+  `nome_produtor` varchar(100) NOT NULL,
+  `ano_safra` date NOT NULL,
+  `descricao` text NOT NULL,
+  `nome_imagem` varchar(100) NOT NULL DEFAULT 'img_placeholder'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `bebida`
+--
+ALTER TABLE `bebida`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `bebida`
+--
+ALTER TABLE `bebida`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
