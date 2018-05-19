90ef725e7732b11cf3db4f197ef921814e345099
Tacio Costa <taciomcosta@gmail.com>
2018-05-19 15:46:51 -0300

    resolves #1 adicionando mais verificação de erros ao Servlet GetImagem

--- src/controllers/GetImagem.java
+++ src/controllers/GetImagem.java
@@ -1,8 +1,8 @@
 package controllers;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.URLConnection;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -15,15 +15,19 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/getImagem")
 public class GetImagem extends HttpServlet {
+	private String nomeCompletoImg;
+	private static String IMG_PLACEHOLDER = "do_not_rename.jpg";
+	
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException 
 	{
-		if (request.getParameter("id") == null)
+		if (request.getParameter("id") == null || request.getParameter("id").isEmpty())
 			return;
 		int id = Integer.parseInt(request.getParameter("id"));
 		/*
 		 * Pode haver problemas ao utilizar o getRealPath(), pois ele retorna um endereço
-		 * da pasta 'fotos' na pasta do Tomcat, que demora para sincronizar com a pasta 'fotos'
+		 * da pasta 'imagens' na pasta do Tomcat, que demora para sincronizar com a pasta 'imagens'
 		 * em WebContent. 
 		 * Portanto, as imagens também devem ser salvas utilizando getRealPath()
 		 */
@@ -31,6 +35,10 @@ public class GetImagem extends HttpServlet {
 		byte[] imagem = recuperarImagemOuDefault(caminhoPastaImagem, id);
 		if (imagem == null)
 			return;
+		response.setContentLength(imagem.length);
+		System.out.println("Content length: " + String.valueOf(imagem.length));
+		response.setContentType(URLConnection.guessContentTypeFromName(this.nomeCompletoImg));
+		System.out.println("Content-type: " + URLConnection.guessContentTypeFromName(this.nomeCompletoImg));
 		response.getOutputStream().write(imagem);
 	}
 	
@@ -40,10 +48,14 @@ public class GetImagem extends HttpServlet {
 		byte[] imagem = null;
 		try {
 			imagem = recuperarImagem(caminhoPasta, id);
-			if (imagem == null)
+			if (imagem == null) {
 				imagem = Files.readAllBytes(Paths.get(caminhoPasta, "do_not_rename.jpg"));
+				this.nomeCompletoImg = IMG_PLACEHOLDER;
+			}
+			System.out.println("Retornando imagem " + this.nomeCompletoImg);
 		} catch (Exception e) {
-				System.err.println("Imagem placeholder (do_not_rename.jpg) não encontrada :(");
+			e.printStackTrace();
+			System.err.println("Imagem placeholder (do_not_rename.jpg) não encontrada :(");
 		}
 		return imagem;
 	}
@@ -54,16 +66,20 @@ public class GetImagem extends HttpServlet {
 		byte[] imagem = null;
 		String strId = String.valueOf(id);
 		File diretorio = new File(caminhoPasta);
-		String nomeImg = null;
+		this.nomeCompletoImg = null;
 		for (String nomeArquivo : diretorio.list()) {
 			if (nomeArquivo.split("\\.")[0].equals(strId))
-				nomeImg = nomeArquivo;
+				this.nomeCompletoImg = nomeArquivo;
 		}
-		Path fullCaminhoImagem = Paths.get(caminhoPasta, nomeImg);
-		System.out.println("Tentando recuperar " + fullCaminhoImagem);
+		if (this.nomeCompletoImg == null)
+			return null;
 		try {
+			Path fullCaminhoImagem = Paths.get(caminhoPasta, this.nomeCompletoImg);
 			imagem = Files.readAllBytes(fullCaminhoImagem);
-		} catch (Exception e) { }
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.err.println("Não foi possível abrir a imagem do produto");
+		}
 		return imagem;
 	}
 }
